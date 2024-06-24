4e78cb3358bc015c912f7e0597818d39791fabb8
JonesJonh <143019996+JonesJonh@users.noreply.github.com>
2024-06-24 12:11:37 -0300

    Create inserindo_dados.sql

---
+++ inserindo_dados.sql
@@ -0,0 +1,6 @@
+INSERT INTO `tbl_pessoas` (`nome`) values ('Alfredo');
+INSERT INTO `tbl_pessoas` (`nome`) values ('Beto');
+INSERT INTO `tbl_pessoas` (`nome`) values ('Charles');
+INSERT INTO `tbl_pessoas` (`nome`) values ('Dinelson');
+INSERT INTO `tbl_pessoas` (`nome`) values ('Eufrates');
+INSERT INTO `tbl_pessoas` (`nome`) values ('Franklin');
